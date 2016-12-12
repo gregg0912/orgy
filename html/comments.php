7721ff10540bccdf8afe94c30854dd74c253234c
Eric Joseph Flores <ericjoseph.flores1@gmail.com>
2016-12-12 15:13:00 +0800

    COMMENTS NOTIF SYSTEM INCLUDED, EDITTED QUERIES ON HOME AND GROUP_PAGE TO FILTER COMMENTED

--- html/comments.php
+++ html/comments.php
@@ -221,7 +221,7 @@
 								<?php 
 								if($_SESSION['user_id'] ==$row['user_id']){?>
 									<form method="post" action=""><button name='delete' class="remove" type="submit" value="<?=$disc_id?>"><span class="glyphicon glyphicon-remove"></span> </button></form>
-									<a href="comments.php?org_id=<?=$_GET['org_id']?>&disc_id=<?=$_GET['disc_id']?>&edit=<?=$row['comment_id']?>#<?=$row['comment_id']?>"><button class="edit"><span class="glyphicon glyphicon-pencil"></span> </button></a>
+									<a href="comments.php?org_id=<?=$_GET['org_id']?>&sort_id=<?=$_GET['sort_id']?>&disc_id=<?=$_GET['disc_id']?>&edit=<?=$row['comment_id']?>#<?=$row['comment_id']?>"><button class="edit"><span class="glyphicon glyphicon-pencil"></span> </button></a>
 								<?php }?>
 								<dl>
 									<dt><p><?=nl2br($body)?></p></dt>
