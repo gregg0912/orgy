5fed5a6344d5ade45bfee025f8aea88a631ce6df
gregg <gmicay123@gmail.com>
2016-12-12 15:24:17 +0800

    editing grouppage

--- html/group_page.php
+++ html/group_page.php
@@ -129,8 +129,10 @@
 		?>
 		<!-- Agent Proxy -->
 	<div id="content">
+		<div class="header">
+			<img class="img-absolute" onerror="this.src = '../images/janina.PNG'" src="<?=$result['photo']?>"/>
 			<h1 class="title"><?=$result['org_name']?></h1>
-			<img onerror="this.src = '../images/janina.PNG'" src="<?=$result['photo']?>"/>
+		</div>
 		<div id="announcements">
 			<div class="page-navigation">
 					<a href="org_members.php?orgID=<?= $orgid ?>" id="members" class="buttoncustom"><?php echo "Members ".$members_count;?></a>
