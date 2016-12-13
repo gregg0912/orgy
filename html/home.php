577a792cfebb654164d9f9917e6fb558d66f119c
Emmanuel <nuj101nuj@gmail.com>
2016-12-13 13:37:10 +0800

    Edited home.php and notif.php

--- html/home.php
+++ html/home.php
@@ -16,6 +16,8 @@
     <link rel="stylesheet" type="text/css" href="../css/main.css">
     <link rel="stylesheet" type="text/css" href="../css/navigation.css">
     <link rel="stylesheet" type="text/css" href="../css/home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
 </head>
 <body>
 <?php
